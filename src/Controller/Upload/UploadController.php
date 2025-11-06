e2ab29c2f99f60e422c0422188781e5e3dbe5428
usmonov-r <usmonovrakhmat@gmail.com>
2025-11-06 21:41:05 +0900

    feat: user authentication created

--- src/Controller/Upload/UploadController.php
+++ src/Controller/Upload/UploadController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Upload;
 
+ use App\Entity\FeedbackResult;
  use App\Message\ProcessPresentationJob;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Doctrine\ORM\EntityManagerInterface;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -14,6 +16,11 @@ use Symfony\Component\Uid\Uuid;
 
 class UploadController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    )
+    {}
+
     #[Route('/api/upload', name: 'api_upload', methods: ['POST'])]
     public function upload(
         Request $request,
@@ -28,6 +35,14 @@ class UploadController extends AbstractController
         }
 
         $jobId = Uuid::v4()->toRfc4122();
+
+        $feedbackResult = new FeedbackResult();
+        $feedbackResult->setJobId($jobId);
+
+        if($this->getUser()) {
+            $feedbackResult->setUser($this->getUser());
+        }
+        $this->entityManager->persist($feedbackResult);
 
         $newFileName = $jobId . '.' . $file->guessExtension();
 
